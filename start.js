b808b944c377323327883661aa0b010ae3130d4b
Your Name <pi@example.com>
2025-06-24 19:37:40 +0300

    `

---
+++ start.js
@@ -0,0 +1,8 @@
+function start() {
+    alert('HTML запускаается...')
+    alert('в console информация о запуске')
+    console.log("start (html, js) start/")
+    console.log("file {test.html, test.js, start.js}")
+    console.log("start.on")
+    console.log("\start;")
+}
---
+++ test.js
@@ -0,0 +1,24 @@
+const title2 = document.getElementsByClassName('form')
+const title3 = document.getElementById('input')
+const submit = document.getElementById('submit')
+const form1 = document.getElementById('form2,1')
+const form2 = document.getElementById('form2,2')
+const form3 = document.getElementById('form2,3')
+const form4 = document.getElementById('form2,4')
+const form5 = document.getElementById('form2,5')
+const form6 = document.getElementById('form2,6')
+const form7 = document.getElementById('form2,7')
+const form8 = document.getElementById('form2,8')
+const form9 = document.getElementById('form2,9')
+const form10 = document.getElementById('form2,10')
+//пустая строка между переменными и действиями
+title3.onclick = function() {
+    alert(`${title2.value}`)
+}
+submit.onclick = function() {
+    alert(`${form1.value} ${form2.value}`)
+    alert(`${form3.value} ${form4.value}`)
+    alert(`${form5.value} ${form6.value}`)
+    alert(`${form7.value} ${form8.value}`)
+    alert(`${form9.value} ${form10.value}`)
+}
