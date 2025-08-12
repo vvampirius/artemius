573dc6e2a3353921a1b888013ac9ce8d1a5ecd2e
Spirit <t-rex.ik@wampi.re>
2025-08-12 14:34:24 +0300

    t-rex.ik

---
+++ dinosaur life.js
@@ -0,0 +1,24 @@
+let date = new Date()
+let dateMonth = date.getMonth()//месяц
+let dateDate = date.getDate()//число
+let dateDay = date.getDay()//день недели
+const I = document.getElementById("I")
+const bathroom = document.getElementById("bathroom")
+const kitchen = document.getElementById("kitchen")
+const living_room = document.getElementById("living_room")
+const bedroom = document.getElementById("bedroom")
+const exit = document.getElementById("exit")
+
+I.onclick = function () {
+    I.style.display = "none"
+}
+bathroom.onclick = function () {
+}
+kitchen.onclick = function () {
+}
+living_room.onclick = function () {
+}
+bedroom.onclick = function () {
+}
+exit.onclick = function () {
+}
--- test.js
+++ test.js
@@ -1,5 +1,3 @@
-const title2 = document.getElementsByClassName('form')
-const title3 = document.getElementById('input')
 const submit = document.getElementById('submit')
 const form1 = document.getElementById('form2,1')
 const form2 = document.getElementById('form2,2')
@@ -12,9 +10,6 @@ const form8 = document.getElementById('form2,8')
 const form9 = document.getElementById('form2,9')
 const form10 = document.getElementById('form2,10')
 //пустая строка между переменными и действиями
-title3.onclick = function() {
-    alert(`${title2.value}`)
-}
 submit.onclick = function() {
     alert(`${form1.value} ${form2.value}`)
     alert(`${form3.value} ${form4.value}`)
