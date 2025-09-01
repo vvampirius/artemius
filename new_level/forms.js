c85aafc4a891f7e07e780c7870334456ee3a9aa2
Yury Paraschenko <vvampirius@gmail.com>
2025-09-01 08:49:57 +0300

    ~

--- new_level/forms.js
+++ new_level/forms.js
@@ -1,6 +1,7 @@
     const express = require('express');
+    const fs = require("fs");
     const app = express();
-
+    //192.168.88.6
     app.get("/start", function(request, response){
     response.send(`
         <!DOCTYPE html>
@@ -10,7 +11,7 @@
                 <title>form</title>
             </head>
             <body>
-                <form action="http://192.168.88.6:100/form" method="post">
+                <form action="http://localhost:100/form" method="post">
                     <input name="code" type="hidden" value="T-REX.ik0024">
                     <input name="username" type="text" placeholder="username">
                     <input name="password" type="password" placeholder="password">
@@ -23,20 +24,49 @@
     app.use(express.urlencoded({ extended: true }));
 
     app.post('/form', (req, res) => {
-          console.log('code:', req.body.code);
-          console.log('Username:', req.body.username);
-          console.log('Password:', req.body.password);
-          res.send("<h1>form on server</h1>")
+        console.log('code:', req.body.code);
+        console.log('Username:', req.body.username);
+        console.log('Password:', req.body.password);
+        res.send("<h1>form on server</h1><form action='http://localhost:100/next' method='post'><input name='next' type='search'><button type='submit'>next</button></form>")
 
-          const fs = require('fs');
-          const filename = 'text.txt';
-          const content = "NaN";
+        const fs = require('fs');
+        const filename = 'text.txt';
+        const content = req.body.username + req.body.password
+        console.log(content)
 
-          fs.writeFile(filename, content, (err) => {
+        fs.writeFile(filename, content, (err) => {
             if (err) {
                 console.error(err)
-                res.send(`<script>console.error("error")</script>`)
             }
-          });
+        });
     });
+    app.post('/next', (req, res) => {
+        console.log(req.body.next);
+        res.send("<form action='http://localhost:100/finish' method='post'><input name='number' type='number' placeholder='1-100'>1<input name='test' type='range' min='1' max='100'>100<button type='submit'>test</button></form>")
+
+        const fs = require('fs');
+        const filename = 'text.txt';
+        const content = req.body.next;
+
+        fs.writeFile(filename, content, (err) => {
+            if (err) {
+                console.error(err)
+            }
+        });
+    })
+    app.post('/finish', (req, res) => {
+        console.log('number:', req.body.number)
+        console.log('range:', req.body.test)
+        res.send("<h1>finish!</h1>")
+
+        const fs = require('fs');
+        const filename = 'text.txt';
+        const content = req.body.number + req.body.test;
+
+        fs.writeFile(filename, content, (err) => {
+            if (err) {
+                console.error(err)
+            }
+        });
+    })
 app.listen(100);
